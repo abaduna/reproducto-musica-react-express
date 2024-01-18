4be355bf5189403a5d704940b06f7d8a7582e242
abaduna~ <artuelrey@gmail.com>
2024-01-18 11:02:52 -0300

    reproductor temrineod

--- cliente/src/components/ComponetsPlayer.js
+++ cliente/src/components/ComponetsPlayer.js
@@ -5,35 +5,51 @@ const ComponetsPlayer = () => {
   const [endpoint, setEndpoint] = useState(`tracks`);
   const { state, getData } = useFecth(endpoint);
   const { data, loading, error } = state;
-  useEffect(() => {
-    getData();
-  
-  }, []);
-  let tracks =[]
+  const [dataMusic,setdataMusic] = useState()
   console.log(data);
-  if (data !== undefined) {
-    console.log(tracks);
-
-    tracks =data?.map((track) => {
-      console.log(track);
-      return {
-        url: track.url,
-        title: track.utr_tracks,
-        tags:[track.category] ,
-      };
-    });   
+  useEffect(() => {
+    const fetchData = async () => {
+      await getData();
+      console.log(data);
+      setdataMusic(data);
+    };
+  
+    fetchData();
+  }, []);
+  
+  let tracks = [];
+  tracks = [
+    {
+      url: 'https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3',
+      title: 'Madza - Chords of Life',
+      tags: ['house']
+    },
+    {
+      url: 'https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3',
+      title: 'Madza - Late Night Drive',
+      tags: ['dnb']
+    },
+    {
+      url: 'https://audioplayer.madza.dev/Madza-Persistence.mp3',
+      title: 'Madza - Persistence',
+      tags: ['dubstep']
+    }
+  ]
+  if (Array.isArray(data) && data.length > 0) {
+    tracks = Array.isArray(data)
+      ? data.map(track => ({
+          url: track.url,
+          title: track.url, // Cambia esto si tienes un campo específico para el título
+          tags:[track.category?.replace(/"/g, '')] 
+        }))
+      : [];
   }
-
-  // const tracks = data?.map((track) => {
-  //   return {
-  //     url: track.utr_tracks,
-  //     title: track.utr_tracks,
-  //     tags: track.category,
-  //   };
-  // });
+console.log(tracks);
+//
   return (
     <>
-      <Player trackList={tracks} />
+      <Player trackList={Array.isArray(dataMusic)> 0 ? dataMusic : tracks}  />
+      
     </>
   );
 };
